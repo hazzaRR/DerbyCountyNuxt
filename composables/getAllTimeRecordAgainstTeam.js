c6e071075931cf69428f5cfc1b989105731bac63
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-02-28 14:19:13 +0000

    added composable function for fetching all time record against specified team

---
+++ composables/getAllTimeRecordAgainstTeam.js
@@ -0,0 +1,43 @@
+export const getAllTimeRecordAgainstTeam = async (team) => {
+
+    const config = useRuntimeConfig();
+    // Define the base URL
+    const baseUrl = `${config.public.API_BASE_URL}/api/match/record`;
+
+    // Create an empty object to store the query parameters
+    const queryParams = {};
+
+    queryParams.team = team;
+
+    const queryString = Object.keys(queryParams)
+      .map(key => `${key}=${encodeURIComponent(queryParams[key])}`)
+      .join('&');
+
+    const finalUrl = queryString ? `${baseUrl}?${queryString}` : baseUrl;
+
+    try {
+      const response = await fetch(finalUrl, {
+        method: "GET",
+        headers: { 
+            "Content-Type": "application/json",
+        },
+      });
+
+
+      const data = await response.json();
+
+
+      if (response.status === 200) {
+        
+        return data;
+    }
+
+    else {
+        return [];
+    }
+
+    } catch (error) {
+      // Handle any errors that occur during the request
+      console.error(error);
+    }
+}
